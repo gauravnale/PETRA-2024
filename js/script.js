974281031e896a1a145f5bac8b21931a8039d8b4
gauravnale <gauravnale28@gmail.com>
2023-10-01 14:45:12 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Load the Banners Section
+$(document).ready(function(){
+        $("#menu-bar-section").load("../menu-bar.html");
+});
+
+// Load the Banners Section
+$(document).ready(function(){
+        $("#banners-section").load("../banner-control.html");
+});
+
+// Load the News Section
+$(document).ready(function(){
+        $("#news-section").load("../news.html");
+});
+
+// Load the Approaching Deadlines Section
+$(document).ready(function(){
+        $("#deadlines-section").load("../approach.html");
+});
+
+// Load the Footer Section
+$(document).ready(function(){
+        $("#footer-section").load("../footer.html");
+});
